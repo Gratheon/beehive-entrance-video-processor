575893f69a30e9d543c9db9848728dfcb4c9593d
Artjom Kurapov <artkurapov@gmail.com>
2024-04-29 02:06:44 +0300

    fix camera to file writing

--- video_camera_server.py
+++ video_camera_server.py
@@ -1,39 +1,52 @@
 import os
 import time
+import datetime
 import cv2
 import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+FPS = 30
+WIDTH_PX = 1280
+HEIGHT_PX = 720
+
+# enable GPU acceleration
+cv2.CAP_GSTREAMER
+
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+
+if (camera.isOpened() == False):  
+    print("Error reading camera") 
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH_PX)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT_PX)
 cv2.namedWindow("Preview", cv2.WINDOW_NORMAL)
 
 try:
     while True:
-        output_file = f'./tmp/video_{time.time()}.mp4'  # Use .mp4 extension
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Specify codec for MP4
-        out = cv2.VideoWriter(output_file, fourcc, 20.0, (640, 480))
+        timestamp = int(datetime.datetime.now().timestamp())
+        output_file = f'./cam/{timestamp}.mp4'
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_file, fourcc, FPS, (WIDTH_PX, HEIGHT_PX))
 
         start_time = time.time()
-        while time.time() - start_time < 10:
+        while True:
             ret, frame = camera.read()
-            if ret:
-                cv2.imshow("Preview", frame)
-                out.write(frame)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
-
-        out.release()
-
-        # upload()
+            if not ret:
+                print('breaking')
+                break
+            out.write(frame)
+            cv2.imshow("Preview", frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q') or time.time() - start_time >= 10:
+                out.release()
+                break
 
 except KeyboardInterrupt:
     print("Recording and uploading stopped by user")
 
 finally:
+    camera.release()
     cv2.destroyAllWindows()
-    camera.close()
 
 def upload():
     # Prepare multipart/form-data request
