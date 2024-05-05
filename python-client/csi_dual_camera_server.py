bc0f6a6802af12e75c1e6c5b19b2099f7e693ef0
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-05-06 01:28:10 +0300

    fix framerate

--- python-client/csi_dual_camera_server.py
+++ python-client/csi_dual_camera_server.py
@@ -1,3 +1,16 @@
+# MIT License
+# Copyright (c) 2019-2022 JetsonHacks
+
+# A simple code snippet
+# Using two  CSI cameras (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit with two CSI ports (Jetson Nano, Jetson Xavier NX) via OpenCV
+# Drivers for the camera and OpenCV are included in the base image in JetPack 4.3+
+
+# This script will open a window and place the camera stream from each camera in a window
+# arranged horizontally.
+# The camera streams are each read in their own thread, as when done sequentially there
+# is a noticeable lag
+
 import os
 import cv2
 import threading
@@ -8,10 +21,15 @@ from uploader import upload
 
 
 class CSI_Camera:
+
     def __init__(self):
+        # Initialize instance variables
+        # OpenCV video capture element
         self.video_capture = None
+        # The last captured image from the camera
         self.frame = None
         self.grabbed = False
+        # The thread where the video capture runs
         self.read_thread = None
         self.read_lock = threading.Lock()
         self.running = False
@@ -21,6 +39,7 @@ class CSI_Camera:
             self.video_capture = cv2.VideoCapture(
                 gstreamer_pipeline_string, cv2.CAP_GSTREAMER
             )
+            # Grab the first frame to start the video capturing
             self.grabbed, self.frame = self.video_capture.read()
 
         except RuntimeError:
@@ -33,6 +52,7 @@ class CSI_Camera:
         if self.running:
             print('Video capturing is already running')
             return None
+        # create a thread to read the camera image
         if self.video_capture != None:
             self.running = True
             self.read_thread = threading.Thread(target=self.updateCamera)
@@ -41,10 +61,12 @@ class CSI_Camera:
 
     def stop(self):
         self.running = False
+        # Kill the thread
         self.read_thread.join()
         self.read_thread = None
 
     def updateCamera(self):
+        # This is the thread to read images from the camera
         while self.running:
             try:
                 grabbed, frame = self.video_capture.read()
@@ -53,6 +75,8 @@ class CSI_Camera:
                     self.frame = frame
             except RuntimeError:
                 print("Could not read image from camera")
+        # FIX ME - stop and cleanup thread
+        # Something bad happened
 
     def read(self):
         with self.read_lock:
@@ -64,8 +88,17 @@ class CSI_Camera:
         if self.video_capture != None:
             self.video_capture.release()
             self.video_capture = None
+        # Now kill the thread
         if self.read_thread != None:
             self.read_thread.join()
+
+
+""" 
+gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+Flip the image by setting the flip_method (most common values: 0 and 2)
+display_width and display_height determine the size of each camera pane in the window on the screen
+Default 1920x1080
+"""
 
 
 def gstreamer_pipeline(
@@ -96,6 +129,8 @@ def gstreamer_pipeline(
     )
 
 FPS = 30
+OUT_FPS = 15 # jetson is not good at writing to disk apparently, so it needs time
+
 WIDTH_PX = 640 # 960 # 640 # 1920
 HEIGHT_PX = 360 # 540 # 480 # 1080
 VIDEO_FILE_MAX_DURATION_SEC = 10
@@ -135,48 +170,70 @@ def run_cameras():
         try:
             # video writing loop
             while True:
+                # write to file
                 timestamp = int(datetime.datetime.now().timestamp())
                 output_file = f'/home/gratheon/Desktop/gratheon/python-client/cam/{timestamp}.mp4'
 
                 writer = cv2.VideoWriter(
                     f'appsrc ! video/x-raw, format=BGR ! videoconvert ! x264enc tune=zerolatency bitrate=3000 speed-preset=superfast ! video/x-h264, stream-format=byte-stream ! h264parse ! mp4mux ! filesink location={output_file}',
-                      cv2.CAP_GSTREAMER, 0, FPS,(WIDTH_PX,HEIGHT_PX))
+                      cv2.CAP_GSTREAMER, 0, OUT_FPS,(WIDTH_PX,HEIGHT_PX))
 
                 if not writer.isOpened():
                     print("Failed to open output")
                     exit()
 
                 start_time = time.time()
+                lastFrameWrite = time.time()
 
                 # camera reading loop
+
+                TIME_TO_FRAME = 0.06
+                lastCycleTime = time.time()
                 while True:
                     _, left_image = left_camera.read()
                     _, right_image = right_camera.read()
+                    # Use numpy to place images next to each other
                     camera_images = np.hstack((left_image, right_image)) 
 
 
                     if left_image is None:
                         break
 
-                    writer.write(left_image)
+                    TIME_TO_FRAME = time.time() - lastCycleTime
+                    isFrameDelayElapsed = time.time() - lastFrameWrite >= (1/OUT_FPS)-TIME_TO_FRAME
+                    lastCycleTime = time.time()
 
+                    if(isFrameDelayElapsed):
+                        print(time.time() - lastFrameWrite)
+                        writer.write(left_image)
+                        lastFrameWrite = time.time()
+
+                    # Check to see if the user closed the window
+                    # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+                    # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
                     if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
                         cv2.imshow(window_title, camera_images)
                     else:
                         break
 
+                    # This also acts as
+                    keyCode = cv2.waitKey(30) & 0xFF
+
                     isVideoLengthReached = time.time() - start_time >= VIDEO_FILE_MAX_DURATION_SEC
 
                     # exit on escape or q
                     if cv2.waitKey(30) & 0xFF == 27 or cv2.waitKey(1) & 0xFF == ord('q') or isVideoLengthReached:
-                        writer.release()
+                        # writer.release()
                         break
 
-                upload(output_file)
+            
+                #upload(output_file)
 
                 # remove file after uploading, you can leave it if you want a local cache
                 # but you need enough storage to not run out of space
-                os.remove(output_file)
+                # os.remove(output_file)
+
+            writer.release()
         finally:
 
             left_camera.stop()
@@ -190,6 +247,7 @@ def run_cameras():
         left_camera.release()
         right_camera.stop()
         right_camera.release()
+
 
 
 if __name__ == "__main__":
