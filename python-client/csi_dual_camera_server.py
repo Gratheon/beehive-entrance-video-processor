8432236f7f65cb925f504d11d06a12043cdfb25f
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-05-06 00:45:00 +0300

    use gstreamer for writing to disk

--- python-client/csi_dual_camera_server.py
+++ python-client/csi_dual_camera_server.py
@@ -6,15 +6,12 @@ import datetime
 import time
 from uploader import upload
 
+
 class CSI_Camera:
     def __init__(self):
-        # Initialize instance variables
-        # OpenCV video capture element
         self.video_capture = None
-        # The last captured image from the camera
         self.frame = None
         self.grabbed = False
-        # The thread where the video capture runs
         self.read_thread = None
         self.read_lock = threading.Lock()
         self.running = False
@@ -24,7 +21,6 @@ class CSI_Camera:
             self.video_capture = cv2.VideoCapture(
                 gstreamer_pipeline_string, cv2.CAP_GSTREAMER
             )
-            # Grab the first frame to start the video capturing
             self.grabbed, self.frame = self.video_capture.read()
 
         except RuntimeError:
@@ -37,7 +33,6 @@ class CSI_Camera:
         if self.running:
             print('Video capturing is already running')
             return None
-        # create a thread to read the camera image
         if self.video_capture != None:
             self.running = True
             self.read_thread = threading.Thread(target=self.updateCamera)
@@ -46,12 +41,10 @@ class CSI_Camera:
 
     def stop(self):
         self.running = False
-        # Kill the thread
         self.read_thread.join()
         self.read_thread = None
 
     def updateCamera(self):
-        # This is the thread to read images from the camera
         while self.running:
             try:
                 grabbed, frame = self.video_capture.read()
@@ -60,8 +53,6 @@ class CSI_Camera:
                     self.frame = frame
             except RuntimeError:
                 print("Could not read image from camera")
-        # FIX ME - stop and cleanup thread
-        # Something bad happened
 
     def read(self):
         with self.read_lock:
@@ -73,17 +64,8 @@ class CSI_Camera:
         if self.video_capture != None:
             self.video_capture.release()
             self.video_capture = None
-        # Now kill the thread
         if self.read_thread != None:
             self.read_thread.join()
-
-
-""" 
-gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
-Flip the image by setting the flip_method (most common values: 0 and 2)
-display_width and display_height determine the size of each camera pane in the window on the screen
-Default 1920x1080
-"""
 
 
 def gstreamer_pipeline(
@@ -127,6 +109,7 @@ def run_cameras():
             capture_width=1920,
             capture_height=1080,
             flip_method=0,
+            framerate=FPS,
             display_width=WIDTH_PX, #960,
             display_height=HEIGHT_PX, #540,
         )
@@ -150,13 +133,18 @@ def run_cameras():
         cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
 
         try:
-
             # video writing loop
             while True:
-                # write to file
                 timestamp = int(datetime.datetime.now().timestamp())
-                output_file = f'./python-client/cam/{timestamp}.mp4'
-                out = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), FPS, (WIDTH_PX, HEIGHT_PX))
+                output_file = f'/home/gratheon/Desktop/gratheon/python-client/cam/{timestamp}.mp4'
+
+                writer = cv2.VideoWriter(
+                    f'appsrc ! video/x-raw, format=BGR ! videoconvert ! x264enc tune=zerolatency bitrate=3000 speed-preset=superfast ! video/x-h264, stream-format=byte-stream ! h264parse ! mp4mux ! filesink location={output_file}',
+                      cv2.CAP_GSTREAMER, 0, FPS,(WIDTH_PX,HEIGHT_PX))
+
+                if not writer.isOpened():
+                    print("Failed to open output")
+                    exit()
 
                 start_time = time.time()
 
@@ -164,28 +152,24 @@ def run_cameras():
                 while True:
                     _, left_image = left_camera.read()
                     _, right_image = right_camera.read()
-                    # Use numpy to place images next to each other
                     camera_images = np.hstack((left_image, right_image)) 
 
-                    # write to file
-                    out.write(left_image)
 
-                    # Check to see if the user closed the window
-                    # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
-                    # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+                    if left_image is None:
+                        break
+
+                    writer.write(left_image)
+
                     if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
                         cv2.imshow(window_title, camera_images)
                     else:
                         break
 
-                    # This also acts as
-                    keyCode = cv2.waitKey(30) & 0xFF
-
                     isVideoLengthReached = time.time() - start_time >= VIDEO_FILE_MAX_DURATION_SEC
 
                     # exit on escape or q
                     if cv2.waitKey(30) & 0xFF == 27 or cv2.waitKey(1) & 0xFF == ord('q') or isVideoLengthReached:
-                        out.release()
+                        writer.release()
                         break
 
                 upload(output_file)
@@ -206,7 +190,6 @@ def run_cameras():
         left_camera.release()
         right_camera.stop()
         right_camera.release()
-
 
 
 if __name__ == "__main__":
